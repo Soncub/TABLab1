dbfa525fd8df2c2d81536020ea64b706ce969625
Andrew Mokhtary <149095320+Soncub@users.noreply.github.com>
2024-08-29 14:14:59 -0400

    Complete Changes (I Think)

--- Assets/Scripts/ChessPiece.cs
+++ Assets/Scripts/ChessPiece.cs
@@ -33,35 +33,35 @@ public class ChessPiece : MonoBehaviour
 		switch (type)
         {
             case PieceType.Pawn:
-				Gizmos.DrawLine(new Vector3(7, 1, 0), new Vector3(7, 3, 0));
+				Gizmos.DrawLine(new Vector3(transform.position.x, transform.position.y, 0), new Vector3(transform.position.x, transform.position.y + 2, 0));
 				break;
             case PieceType.Rook:
-				Gizmos.DrawLine(new Vector3(-2, 7, 0), new Vector3(2, 7, 0));
-				Gizmos.DrawLine(new Vector3(0, 5, 0), new Vector3(0, 9, 0));
+				Gizmos.DrawLine(new Vector3(transform.position.x - 8, transform.position.y, 0), new Vector3(transform.position.x + 8, transform.position.y, 0));
+				Gizmos.DrawLine(new Vector3(transform.position.x, transform.position.y - 8, 0), new Vector3(transform.position.x, transform.position.y + 8, 0));
 				break;
             case PieceType.Bishop:
-				Gizmos.DrawLine(new Vector3(3, -2, 0), new Vector3(7, 2, 0));
-				Gizmos.DrawLine(new Vector3(3, 2, 0), new Vector3(7, -2, 0));
+				Gizmos.DrawLine(new Vector3(transform.position.x - 8, transform.position.y - 8, 0), new Vector3(transform.position.x + 8, transform.position.y + 8, 0));
+				Gizmos.DrawLine(new Vector3(transform.position.x - 8, transform.position.y + 8, 0), new Vector3(transform.position.x + 8, transform.position.y - 8, 0));
 				break;
             case PieceType.Knight:
-				Gizmos.DrawLine(new Vector3(3, 5, 0), new Vector3(7, 5, 0));
-				Gizmos.DrawLine(new Vector3(5, 3, 0), new Vector3(5, 7, 0));
-				Gizmos.DrawLine(new Vector3(4, 7, 0), new Vector3(6, 7, 0));
-				Gizmos.DrawLine(new Vector3(4, 3, 0), new Vector3(6, 3, 0));
-				Gizmos.DrawLine(new Vector3(3, 4, 0), new Vector3(3, 6, 0));
-				Gizmos.DrawLine(new Vector3(7, 4, 0), new Vector3(7, 6, 0));
+				Gizmos.DrawLine(new Vector3(transform.position.x - 2, transform.position.y, 0), new Vector3(transform.position.x + 2, transform.position.y, 0));
+				Gizmos.DrawLine(new Vector3(transform.position.x, transform.position.y - 2, 0), new Vector3(transform.position.x, transform.position.y + 2, 0));
+				Gizmos.DrawLine(new Vector3(transform.position.x - 2, transform.position.y - 1, 0), new Vector3(transform.position.x - 2, transform.position.y + 1, 0));
+				Gizmos.DrawLine(new Vector3(transform.position.x + 2, transform.position.y - 1, 0), new Vector3(transform.position.x + 2, transform.position.y + 1, 0));
+				Gizmos.DrawLine(new Vector3(transform.position.x - 1, transform.position.y + 2, 0), new Vector3(transform.position.x + 1, transform.position.y + 2, 0));
+				Gizmos.DrawLine(new Vector3(transform.position.x - 1, transform.position.y - 2, 0), new Vector3(transform.position.x + 1, transform.position.y - 2, 0));
 				break;
             case PieceType.King:
-				Gizmos.DrawLine(new Vector3(1, 2, 0), new Vector3(3, 2, 0));
-				Gizmos.DrawLine(new Vector3(2, 1, 0), new Vector3(2, 3, 0));
-				Gizmos.DrawLine(new Vector3(1, 3, 0), new Vector3(3, 1, 0));
-				Gizmos.DrawLine(new Vector3(1, 1, 0), new Vector3(3, 3, 0));
+				Gizmos.DrawLine(new Vector3(transform.position.x - 1, transform.position.y, 0), new Vector3(transform.position.x + 1, transform.position.y, 0));
+				Gizmos.DrawLine(new Vector3(transform.position.x, transform.position.y - 1, 0), new Vector3(transform.position.x, transform.position.y + 1, 0));
+				Gizmos.DrawLine(new Vector3(transform.position.x - 1, transform.position.y - 1, 0), new Vector3(transform.position.x + 1, transform.position.y + 1, 0));
+				Gizmos.DrawLine(new Vector3(transform.position.x - 1, transform.position.y + 1, 0), new Vector3(transform.position.x + 1, transform.position.y - 1, 0));
 				break;
             case PieceType.Queen:
-				Gizmos.DrawLine(new Vector3(0, 5, 0), new Vector3(4, 5, 0));
-				Gizmos.DrawLine(new Vector3(2, 3, 0), new Vector3(2, 7, 0));
-				Gizmos.DrawLine(new Vector3(0, 7, 0), new Vector3(4, 3, 0));
-				Gizmos.DrawLine(new Vector3(0, 3, 0), new Vector3(4, 7, 0));
+				Gizmos.DrawLine(new Vector3(transform.position.x - 8, transform.position.y, 0), new Vector3(transform.position.x + 8, transform.position.y, 0));
+				Gizmos.DrawLine(new Vector3(transform.position.x, transform.position.y - 8, 0), new Vector3(transform.position.x, transform.position.y + 8, 0));
+				Gizmos.DrawLine(new Vector3(transform.position.x - 8, transform.position.y - 8, 0), new Vector3(transform.position.x + 8, transform.position.y + 8, 0));
+				Gizmos.DrawLine(new Vector3(transform.position.x - 8, transform.position.y + 8, 0), new Vector3(transform.position.x + 8, transform.position.y - 8, 0));
 				break;
         } 
     }
